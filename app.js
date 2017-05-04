646f66fc6b6af09a8ffa75528e54101ac4873179
shurnster <slainuponmyshield@gmail.com>
2017-05-04 15:10:21 -0700

    added new files and folders

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './main.scss';
+import Demo from './components/demo';
 
-class App extends React.Component {
-  render() {
-    return (<h1 className="text-center">Basic React Boilerplate</h1>)
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Demo />, document.getElementById('root'));
---
+++ components/demo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../styles/main.scss';
+
+class Demo extends React.Component {
+  render() {
+    return (<h1 className="text-center">Basic React Boilerplate</h1>)
+  }
+}
+
+export default Demo;
