46baabc1bc4a4624ae605f50f34ba5da878dfcf7
shurnster <slainuponmyshield@gmail.com>
2017-06-22 17:40:21 -0700

    added props for Search Bar

--- components/demo.js
+++ components/demo.js
@@ -2,22 +2,26 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import HeroTable from './Hero_Table.js';
 import '../styles/main.scss';
+import PropTypes from 'prop-types';
 
 class Demo extends React.Component {
   constructor(props) {
     super(props);
     this.displayName = 'SuperheroFeats';
   }
-
   render() {
     return (
       <div className="app-root">
         <h1>Superhero Feats</h1>
-        <SearchBar />
+        <SearchBar data={this.props.showData} />
         <HeroTable data={this.props.showData} />
       </div>
     );
   }
 }
+
+Demo.propTypes = {
+  showData: PropTypes.array
+};
 
 export default Demo;
