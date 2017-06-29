6e89df9f2b610be9fae56fda1c170be0bad6c9b7
shurnster <slainuponmyshield@gmail.com>
2017-06-28 23:07:09 -0700

    created a req handler for images

--- index-server.js
+++ index-server.js
@@ -6,10 +6,14 @@ const morgan = require('morgan');
 const app = express();
 const PORT = 8000;
 const filePath = path.resolve(__dirname, 'dist/');
+const imagePath = path.resolve(__dirname, 'images/');
 
 //Use nunchucks as the template engine
 app.set('view engine', 'njk');
-nunjucks.configure('views', {express: app, autoescape: true});
+nunjucks.configure('views', {
+  express: app,
+  autoescape: true
+});
 
 /**
 * @method getTemplate
@@ -25,10 +29,16 @@ app.use(morgan('dev'));
 
 //Use the handler to retrieve the static files.
 app.use(express.static(filePath));
+app.use(express.static(imagePath));
 
 //Render the index template upon the default root index request.
 app.get('/', (req, res) => {
   res.render(getTemplate('index'));
+});
+
+//Send images based on the GET request.
+app.get(/^\/.*\.png$/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'images/row_background_4_03.png'));
 });
 
 app.listen(PORT, () => console.log('The application is listening to port, ' + PORT));
