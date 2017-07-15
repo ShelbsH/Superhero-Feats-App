521c472a57e8ea4f7c175370e2c5da61020082bd
shurnster <slainuponmyshield@gmail.com>
2017-07-14 23:49:27 -0700

    added state for character page

--- components/Hero_Table.js
+++ components/Hero_Table.js
@@ -1,23 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import HeroPage from './Hero_Page';
 import '../styles/components/Hero_Table.scss';
 
 class HeroTable extends React.Component {
   constructor(props) {
     super(props);
+
+    this.heroHandler = this
+      .heroHandler
+      .bind(this);
+    this.state = {
+      heroClicked: false
+    }
+  }
+
+  heroHandler(evt) {
+    this.props.showHeroData(evt.currentTarget.getAttribute('data-index'));
+    this.setState({heroClicked: true});
   }
 
   render() {
     const bodyCol = 'heroTable-body-col';
     const spanImg = 'heroTable-body-col-span-img';
 
-    const td = children => (<td className={bodyCol}>{children}</td>);
+    const td = children => (
+      <td className={bodyCol}>{children}</td>
+    );
 
     const render_table = props => {
 
       return props.map((items, idx) => {
         return (
-          <tr key={idx} className="heroTable-body-row">
+          <tr
+            key={idx}
+            className="heroTable-body-row"
+            data-index={idx}
+            onClick={this.heroHandler}>
             <td className={bodyCol}>
               <span><img className={spanImg} src={items.thumbnail}/></span>{items.real_name}
             </td>
@@ -30,22 +49,27 @@ class HeroTable extends React.Component {
     };
 
     const headerClass = 'heroTable-header';
-    const th = childText => (<th className={headerClass}>{childText}</th>);
+    const th = childText => (
+      <th className={headerClass}>{childText}</th>
+    );
 
     return (
-      <table className="heroTable">
-        <thead className="heroTable-head">
-          <tr>
-            {th('Real Name')}
-            {th('Super Name')}
-            {th('Tier Level')}
-            {th('Publisher')}
-          </tr>
-        </thead>
-        <tbody className="heroTable-body">
-          {render_table(this.props.data)}
-        </tbody>
-      </table>
+      <div>
+        <table className="heroTable">
+          <thead className="heroTable-head">
+            <tr>
+              {th('Real Name')}
+              {th('Super Name')}
+              {th('Tier Level')}
+              {th('Publisher')}
+            </tr>
+          </thead>
+          <tbody className="heroTable-body">
+            {render_table(this.props.data)}
+          </tbody>
+        </table>
+        <HeroPage displayHero={this.props.displayHero} />
+      </div>
     );
   }
 }
--- components/demo.js
+++ components/demo.js
@@ -10,7 +10,25 @@ class Demo extends React.Component {
   constructor(props) {
     super(props);
     this.displayName = 'SuperheroFeats';
+
+    this.showHeroHandler = this
+      .showHeroHandler
+      .bind(this);
+
+    this.state = {
+      showHero: null,
+      heroData: null
+    };
   }
+
+  showHeroHandler(index) {
+    let getHeroData = this.props.showData[index];
+    this.setState({
+      showHero: index,
+      heroData: Object.values(getHeroData)
+    });
+  }
+
   render() {
     return (
       <div className="app-root">
@@ -22,7 +40,7 @@ class Demo extends React.Component {
             <SearchBar data={this.props.showData}/>
           </div>
         </nav>
-        <HeroTable data={this.props.showData}/>
+        <HeroTable data={this.props.showData} showHeroData={this.showHeroHandler} displayHero={this.state.heroData}/>
       </div>
     );
   }
