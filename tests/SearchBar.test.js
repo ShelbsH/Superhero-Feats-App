86f257104e5d72766e410204ab17eafc41016b3b
shurnster <slainuponmyshield@gmail.com>
2017-07-22 16:46:45 -0700

    fixed spec issue to correspond with updated autocomplete output

--- tests/SearchBar.test.js
+++ tests/SearchBar.test.js
@@ -62,7 +62,7 @@ describe('The Search Bar Component will display data from Superheroes', () => {
     });
     expect(wrapper.state().keyValues).toBe('clark');
     expect(wrapper.find('li').length).toBe(1);
-    expect(wrapper.find('li').text()).toBe('Clark Kent');
+    expect(wrapper.find('li').text()).toBe('Clark Kent as Superman');
 
   });
 });
