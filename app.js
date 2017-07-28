8c2ff8ef85ededec24e3c396edd7efb13e1d6e5a
shurnster <slainuponmyshield@gmail.com>
2017-07-27 23:31:19 -0700

    added profile pics

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const data = [
     {
       thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957182-superman_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978025-superman_profile.jpg',
       real_name: 'Clark Kent',
       superhero_name: 'Superman',
       tier: 'Power House',
@@ -16,6 +17,7 @@ document.addEventListener('DOMContentLoaded', () => {
       id: '1'
     }, {
       thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957179-batman_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978026-batman_profile.jpg',
       real_name: 'Bruce Wayne',
       superhero_name: 'Batman',
       tier: 'Street Leveler',
@@ -23,6 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
       id: '2'
     }, {
       thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957181-spider-man_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978024-spider-man_profile.jpg',
       real_name: 'Peter Parker',
       superhero_name: 'Spider-Man',
       tier: 'Street Levevler',
@@ -30,6 +33,7 @@ document.addEventListener('DOMContentLoaded', () => {
       id: '3'
     }, {
       thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957189-carol_danvers_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978033-carol_danversl_profile.jpg',
       real_name: 'Carol Danvers',
       superhero_name: 'Captain Marvel',
       tier: 'Power House',
@@ -37,6 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
       id: '4'
     }, {
       thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957180-lantern_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978023-green_lantern_profile.jpg',
       real_name: 'John Stewart',
       superhero_name: 'Green Lantern',
       tier: 'Power House',
@@ -44,6 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
       id: '5'
     }, {
       thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957193-daredevil_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978022-daredevil_profile.jpg',
       real_name: 'Matthew Murdock',
       superhero_name: 'Daredevil',
       tier: 'Street Levevler',
