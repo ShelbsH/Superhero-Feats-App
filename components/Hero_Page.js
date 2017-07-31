66b42e143296419b80605a822f9b7b11c611d3a0
shurnster <slainuponmyshield@gmail.com>
2017-07-30 22:30:57 -0700

    refactored and added state for animations

--- components/Hero_Page.js
+++ components/Hero_Page.js
@@ -5,42 +5,54 @@ import '../styles/components/Hero_Page.scss';
 class HeroPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      click: false,
+      animateSidebar: '',
+      animateMenuIcon: ''
+    };
+
+    this.onSidebarMenuClick = this.onSidebarMenuClick.bind(this);
+  }
+
+  onSidebarMenuClick() {
+
+    this.setState({
+      click: true,
+      animateSidebar: ' heroSidebar-animate',
+      animateMenuIcon: ' menuIconDiv-animate'
+    });
   }
 
   render() {
-    const categoryList = [
-      'Stength/Power',
-      'Speed/Agility',
-      'Martial Arts/Training',
-      'Skills/Fights',
-      'Durability/Endurance',
-      'Misc'
-    ];
+    const addAnimateSidebar = this.state.click ? this.state.animateSidebar : '';
+    const addAnimateMenuIcon = this.state.click ? this.state.animateMenuIcon : '';
 
     const HeroCategoryList = ({featsCategory}) => <li className="heroSidebar-li">{featsCategory}</li>;
 
     const HeroCategorySidebar = () => {
-      return (
-        <div className="heroSidebar-profile-category">
-          <ul className="heroSidebar-ul">
-            {categoryList.map((list, index) => <HeroCategoryList key={index} featsCategory={list} />)}
-          </ul>
-        </div>
-      );
-    };
+      const categoryList = [
+        'Stength/Power',
+        'Speed/Agility',
+        'Martial Arts/Training',
+        'Skills/Fights',
+        'Durability/Endurance',
+        'Misc'
+      ];
 
-    const HeroIconSidebar = ({iconPng}) => {
       return (
-        <div className="heroSidebar-profile-icon">
-          <img src={iconPng} />
+        <div className="heroSidebar-category">
+          <ul className="heroSidebar-ul">
+            {categoryList.map((list, index) => <HeroCategoryList key={index} featsCategory={list}/>)}
+          </ul>
         </div>
       );
     };
 
     const CharacterInfoSidebar = props => {
       return (
-        <div className="heroSidebar-profile-char">
-          <img className="heroSidebar-profile-img" src={props.profile_picture} />
+        <div className="heroSidebar-characterDetail">
+          <img className="heroSidebar-img" src={props.profile_picture}/>
           <h2 className="heroSidebar-h2">{props.real_name}</h2>
           <h2 className="heroSidebar-h2">As</h2>
           <h2 className="heroSidebar-h2">{props.superhero_name}</h2>
@@ -51,38 +63,39 @@ class HeroPage extends React.Component {
     const HeroSidebar = props => {
       return (
         <div>
-          <nav className="heroSidebar">
-            <div className="heroSidebar-profile">
-              <HeroIconSidebar iconPng="../images/sidebar_icon.png" />
-              <CharacterInfoSidebar
-                real_name={props.real_name}
-                superhero_name={props.superhero_name}
-                profile_picture={props.profile_picture} />
-              <HeroCategorySidebar />
-            </div>
-          </nav>
+          <div className="heroSidebar-profile">
+            <CharacterInfoSidebar
+              real_name={props.real_name}
+              superhero_name={props.superhero_name}
+              profile_picture={props.profile_picture}/>
+            <HeroCategorySidebar/>
+          </div>
         </div>
       );
     };
 
     const render_HeroSidebar = props => {
-      if(Array.isArray(props)) {
+      if (Array.isArray(props)) {
         return props.map((lists, index) => {
           return (
             <HeroSidebar
               key={index}
               real_name={lists.real_name}
               superhero_name={lists.superhero_name}
-              profile_picture={lists.profile_picture} />
-          );
+              profile_picture={lists.profile_picture}/>);
         });
       }
     };
-      
+
     return (
       <div className="heroPage">
         <div className="wrapper">
-          {render_HeroSidebar(this.props.heroPageData)}
+          <nav className={'heroSidebar' + addAnimateSidebar}>
+            {render_HeroSidebar(this.props.heroPageData)}
+          </nav>
+          <div className={'heroSidebar-menuIconDiv' + addAnimateMenuIcon}>
+            <img src="../images/sidebar_icon.png" onClick={this.onSidebarMenuClick}/>
+          </div>
         </div>
       </div>
     );
