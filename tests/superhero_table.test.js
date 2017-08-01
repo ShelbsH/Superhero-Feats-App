0f02602ddce3977239643c1155853e3caa8b66c1
shurnster <slainuponmyshield@gmail.com>
2017-08-01 10:13:42 -0700

    modified for stateless components and added sidebar testing specs

--- tests/superhero_table.test.js
+++ tests/superhero_table.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import HeroTable from '../components/Hero_Table';
 import HeroPage from '../components/Hero_Page';
 
@@ -7,84 +7,87 @@ describe('Superhero Table', () => {
   let wrapper;
   let superhero_data;
   let firstRowNode;
+  let HeroBody;
+  let supermanData;
 
   superhero_data = [
     {
-      thumbnail: 'http://tse2.mm.bing.net/th?id=OIP.zQxxTe2BewqNhEF6_FEVmQEJEs&pid=15.1',
+      thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957182-superman_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978025-superman_profile.jpg',
       real_name: 'Clark Kent',
       superhero_name: 'Superman',
       tier: 'Power House',
-      publisher: 'DC'
+      publisher: 'DC',
+      id: '1'
     }, {
-      thumbnail: 'http://tse2.mm.bing.net/th?id=OIP.zQxxTe2BewqNhEF6_FEVmQEJEs&pid=15.1',
+      thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957179-batman_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978026-batman_profile.jpg',
       real_name: 'Bruce Wayne',
       superhero_name: 'Batman',
       tier: 'Street Leveler',
-      publisher: 'DC'
+      publisher: 'DC',
+      id: '2'
     }, {
-      thumbnail: 'http://tse2.mm.bing.net/th?id=OIP.zQxxTe2BewqNhEF6_FEVmQEJEs&pid=15.1',
+      thumbnail: 'https://static.comicvine.com/uploads/original/13/136345/5957181-spider-man_avatar.jpg',
+      profile_picture: 'https://static.comicvine.com/uploads/original/13/136345/5978024-spider-man_profile.jpg',
       real_name: 'Peter Parker',
       superhero_name: 'Spider-Man',
       tier: 'Street Levevler',
-      publisher: 'Marvel'
+      publisher: 'Marvel',
+      id: '3'
     }
   ];
 
-  wrapper = shallow(<HeroTable data={superhero_data}/>);
+  wrapper = mount(<HeroTable data={superhero_data}/>);
+  supermanData = superhero_data[0];
 
   // Get the first node to retrieve the collections of "td" elements to match the
   // length the "superhero_data" array for testing.
   firstRowNode = wrapper
     .find('tbody')
-    .find('tr')
-    .first()
-    .children();
+    .find('td.heroTable-body-col');
+    
 
   it('should be able to match the equal amount of properties from the superhero_data', () => {
-    expect(firstRowNode).toHaveLength(4);
+    const getFirstRow = wrapper.find('tbody').find('td');
+    expect(getFirstRow.debug());
+    expect(getFirstRow.children()).toHaveLength(3);
   });
 
   it('needs to successfully render the thumbnail image', () => {
 
     //Get the image within the <td> element and assert for the "src" attribute.
-    const getImg = firstRowNode
-      .find('img')
-      .prop('src');
-
-    expect(getImg).toBe(superhero_data[0].thumbnail);
+    expect(firstRowNode.first().find('img').prop('src')).toBe(supermanData.thumbnail);
   });
 
   it('needs to successfully render the real name', () => {
-    expect(firstRowNode.at(0).text()).toBe(superhero_data[0].real_name);
+    expect(firstRowNode.at(0).text()).toBe(supermanData.real_name);
   });
 
   it('needs to successfully render the superhero name', () => {
-
-    expect(firstRowNode.at(1).text()).toBe(superhero_data[0].superhero_name);
+    expect(firstRowNode.at(1).text()).toBe(supermanData.superhero_name);
   });
 
   it('needs to successfully render the tier level', () => {
-    expect(firstRowNode.at(2).text()).toBe(superhero_data[0].tier);
+    expect(firstRowNode.at(2).text()).toBe(supermanData.tier);
   });
 
   it('needs to successfully render the publisher', () => {
-    expect(firstRowNode.at(3).text()).toBe(superhero_data[0].publisher);
+    expect(firstRowNode.at(3).text()).toBe(supermanData.publisher);
   });
 
   describe('HeroPage Component', () => {
+    const mockTargetIndex = 2;
+    const mockState = [superhero_data[mockTargetIndex]];
+    const tableWrapper = shallow(<HeroTable data={superhero_data} heroPageData={mockState}/>);
+    const heroPageWrapper = mount(<HeroPage heroPageData={mockState} />);
+    const spiderManData = superhero_data[2];
 
-    it('should be able to load up the HeroPage component when a row is clicked', () => {
-      const mockTargetIndex = 2;
-      const mockState = [superhero_data[mockTargetIndex]];
-      const tableWrapper = shallow(<HeroTable data={superhero_data} heroPageData={mockState}/>);
-      const heroPageWrapper = shallow(<HeroPage heroPageData={mockState} />);
-
-      const tr = tableWrapper
+    const tr = tableWrapper
         .find('tbody')
         .find('tr')
         .at(0);
       
-
       tr.simulate('click', {
         currentTarget: {
           dataset: {
@@ -93,8 +96,17 @@ describe('Superhero Table', () => {
         }
       });
 
+    it('should be able to load up the HeroPage component when a row is clicked', () => {
       expect(heroPageWrapper.exists()).toBe(true);
-      expect(heroPageWrapper.find('li').at(1).text()).toEqual('Peter Parker');
+    });
+
+    it('needs to have the character\'s profile picture image on the sidebar', () => {
+      expect(heroPageWrapper.find('img.heroSidebar-img').prop('src')).toBe(spiderManData.profile_picture)
+    });
+
+    it('needs to have a working character profile on the sidebar', () => {
+      expect(heroPageWrapper.find('h2.heroSidebar-h2').at(0).text()).toEqual(spiderManData.real_name);
+      expect(heroPageWrapper.find('h2.heroSidebar-h2').at(2).text()).toEqual(spiderManData.superhero_name);
     });
   })
 });
