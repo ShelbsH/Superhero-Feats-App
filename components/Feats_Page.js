763eb3103c140150e0d5995d6fc103138078f549
shurnster <slainuponmyshield@gmail.com>
2017-08-03 16:18:47 -0700

    added Feats Page component

---
+++ components/Feats_Page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../styles/components/Feats_Page.scss';
+
+class FeatsPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1 className="page-header header-h1">{this.props.category}feats</h1>
+      </div>
+    );
+  }
+}
+
+FeatsPage.propTypes = {
+  category: PropTypes.string
+};
+
+export default FeatsPage;
