25c40cab3db3b0d93c2dd9779b8cab14c3301c38
shurnster <slainuponmyshield@gmail.com>
2017-08-02 22:31:35 -0700

    added Promise object to attach data with the demo component

--- app.js
+++ app.js
@@ -7,12 +7,35 @@ import Demo from './components/demo';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  // http://localhost:8000/api/heroes
-  // GET /api/heroes
-  const data = [
+  const getJSON = url => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', url);
 
-  ];
+      xhr.onreadystatechange = () => {
 
-  ReactDOM.render(
-    <Demo showData={data}/>, document.getElementById('root'));
+        if (xhr.readyState < 3) {
+          return;
+        }
+
+        if (xhr.status === 302) {
+          return;
+        }
+
+        if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 400) {
+          resolve(JSON.parse(xhr.responseText));
+        }
+
+        if (xhr.readyState === 4 && xhr.statusCode >= 400) {
+          reject('The requested GET URL has failed');
+        }
+      };
+
+      xhr.send();
+    });
+  };
+
+  getJSON('./api/heroes').then((res) => {
+    ReactDOM.render(<Demo showData={res} />, document.getElementById('root'));
+  }).catch(err => console.log(err));
 });
