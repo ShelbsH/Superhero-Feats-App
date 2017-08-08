9ce4053c2337fa3f774960516edbd4e8b1a3f0a8
shurnster <slainuponmyshield@gmail.com>
2017-08-08 16:25:49 -0700

    added pictures via grid gallery component

--- components/Feats_Page.js
+++ components/Feats_Page.js
@@ -1,20 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Gallery from 'react-grid-gallery';
 import '../styles/components/Feats_Page.scss';
 
 class FeatsPage extends React.Component {
 
   render() {
+    const category = this.props.category;
+
+    const FeatsDescription = ({title, description}) => {
+      return (
+        <div>
+          <h2 className="header-h2">Title: {title}</h2>
+          <p>Description: {description}</p>
+        </div>
+      );
+    };
+
+    const render_FeatsDescription = props => {
+      return props.map((lists, index) => {
+        if(lists.feats[category]) {
+          return (
+            <div key={index}>
+              <FeatsDescription
+                title={lists.feats[category].title} 
+                description={lists.feats[category].description}/>
+              <Gallery images={lists.feats[category].images} backdropClosesModal={true}/>
+            </div>
+          );
+        }
+      });
+    };
+
     return (
+      //The image gallery would go on here and would be based off of a state
       <div>
-        <h1 className="page-header header-h1">{this.props.category} feats</h1>
+        <h1 className="page-header header-h1">{category} feats</h1>
+        {render_FeatsDescription(this.props.feats)}
       </div>
     );
   }
 }
 
 FeatsPage.propTypes = {
-  category: PropTypes.string
+  category: PropTypes.string,
+  feats: PropTypes.array
 };
 
 export default FeatsPage;
